bcc8a6f149f7b0e6565b6ad3010130fd6df1fb80
xieyezi <1435398529@qq.com>
2023-11-14 16:21:34 +0800

    fix: deploy failed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: "vercel-edge"
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    typeCheck: true
+  },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
   hooks: {
     "components:dirs": (dirs) => {
@@ -10,11 +19,5 @@ export default defineNuxtConfig({
         pathPrefix: false
       });
     }
-  },
-  devtools: {
-    enabled: true
-  },
-  typescript: {
-    typeCheck: true
   }
 });
